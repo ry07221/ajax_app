ff57c388c20447e4b144ed649ac9786740a6de19
ry <zijoyrb@gamil.com>
2020-12-17 10:01:10 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  # def new
+  # end
+
+  def create
+    post = Post.create(content: params[:content])
+    # render json:{ post: post }
+  end
+  
+end
